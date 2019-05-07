1c148ccdcaa8a87d0b371f1fdcf5911c9c1afb56
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-07 14:59:44 -0400

    update us formats.

--- us_formatting.js
+++ us_formatting.js
@@ -1,36 +1,25 @@
-const US_Format = {
-  FULL_LINE: "MMMM Do YYYY, h:mm:ss a", /* May 7th 2019, 12:17:00 pm */
-  CALENDAR_DATE: "MM/DD/YYYY", /* 04/27/2019 */
-  MONTH_DAY_YEAR: "MMM Do YY", /* May 7th 19 */
-  REGULAR_TIME_UPPERCASE: "h:mm A", /* 7:32 AM */
-  REGULAR_TIME_LOWERCASE: "h:mm a", /* 7:32 AM */
-  MILITARY_TIME: "h:mm"             /* 07:32 */ 
-}
 const moment = require('moment');
+
+const US_Format = {
+  REGULAR_TIME_LC: "h:mm a",    /* 7:32 am */
+  REGULAR_TIME_UC: "h:mm A",    /* 7:32 AM */
+  
+  Date_Calendar: "MM/DD/YYYY",  /* 04/27/2019 */
+  Month_Day_Year: "MMMM D YYYY", /* May 7, 2019 */
+  Month_Day_Year_Time: "MMMM Do YYYY, h:mm:ss a", /* May 7th 2019, 12:17:00 pm */
+  Day_Of_Week_Month_Day_Year_Time: "dddd, MMMM Do YYYY hh:mm a" /* Friday, May 16th 2014 12:30 pm */
+}
+const Additional_Format = {
+  MILITARY_TIME: "h:mm"         /* 12:32 */ 
+}
+
 console.log({
   Regular_Time: "12:43 pm |h:mm a",
   Time_With_Seconds: "12:43:33 PM | hh:mm:ss A",
   Date_Numeric: "05/07/2019 | MM/DD/YYYY",
   Month_Day_Year: "May 7, 2019 | MMMM D YYYY",
   Month_Day_Year_Time: "May 7, 2019 12:43 PM | MMMM D YYYY hh:mm:ss A",
-  Day_Of_Week_Month_Day_Year_Time: "Tuesday, August 7, 2019 12:43 PM | DD MMMM YYYY hh:mm:ss A"
+  Day_Of_Week_Month_Day_Year_Time: "Tuesday, August 7, 2019 12:43 PM | DD MMMM YYYY hh:mm:ss A",
+  US_Format,
+  Additional_Format
 })
-
-// // Format Dates
-// moment().format("MMMM Do YYYY, h:mm:ss a"); /* May 7th 2019, 12:17:00 pm */
-// moment().format("dddd");                    // Tuesday
-// moment().format("MM/DD/YYYY");              // 04/27/2019
-// moment().format("MMM Do YY");               // May 7th 19
-// moment().format();                          // 2019-05-07T12:17:00-04:00
-//                                             // undefined
-// // Relative Time
-// moment("20111031", "YYYYMMDD").fromNow(); // 8 years ago
-// moment("20120620", "YYYYMMDD").fromNow(); // 7 years ago
-// moment().startOf('day').fromNow();        // 12 hours ago
-// moment().endOf('day').fromNow();          // in 12 hours
-// moment().startOf('hour').fromNow();       // 18 minutes ago
-                         
-// // Calendar Time
-// moment().subtract(10, 'days').calendar().format("MM/DD/YYYY"); // 04/17/2019
-
-// moment().subtract(6, 'days').calendar();  // Last Wednesday at 12:18 PM
